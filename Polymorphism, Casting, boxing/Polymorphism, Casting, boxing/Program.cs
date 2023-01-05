3d0b03b67e6e75ef61e996b906698cd3b4eb0fda
Ramil <ramilev@code.edu.az>
2023-01-05 23:54:17 +0400

    homework added

---
+++ Polymorphism, Casting, boxing/Polymorphism, Casting, boxing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polymorphism__Casting__boxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
